2ab4a3a2ee764cdb49b056e81283bcc745ff57ac
Heitor Amaral <heitor.amaral.santos@gmail.com>
2020-08-10 22:02:07 -0300

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { PanoramaView } from "@lightbase/react-native-panorama-view";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <PanoramaView
+        style={{ flex: 1 }}
+        dimensions={{
+          height: Dimensions.get("window").height,
+          width: Dimensions.get("window").width
+        }}
+        inputType="mono"
+        // imageUrl="http://localhost:53469/IMG_20200130_104908_00_merged.jpg"
+        imageUrl="https://raw.githubusercontent.com/googlevr/gvr-android-sdk/master/assets/panoramas/testRoom1_2kMono.jpg"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +24,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
